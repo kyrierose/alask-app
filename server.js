bbc16c0a6f9f3d03da0b60d562fa360cccd05a58
Akshit Dhar <akshitdhar@live.com>
2018-07-25 00:25:11 +0530

    Defining auth routes

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const PORT = 3000 || process.env.PORT
 
 const app = express()
 
+app.use(bodyParser.apply.json())
+app.use(cors())
+app.use(bodyParser.urlencoded({extended:true}))
+
 app.get('/',()=>res.send("Application running."))
+
+app.get('**', ()=>res.send("Error 404: You seems to have lost."))
 
 app.listen(PORT,()=>{
     console.log(`Listening at PORT ${PORT}`)
